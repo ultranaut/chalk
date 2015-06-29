0226cf62191daa2114b0beb5f34e1b4c1313f4aa
John Shearer <john@ultranaut.com>
2015-06-29 17:28:05 -0400

    Move socket.io code inside of react code

--- web/src/js/jsx/app.js
+++ web/src/js/jsx/app.js
@@ -1,21 +1,72 @@
 /* global React */
 'use strict';
 
-var App = React.createClass({  // eslint-disable-line no-unused-vars
+
+var App = React.createClass({   // eslint-disable-line no-unused-vars
   getInitialState: function () {
     return {
-      messages: [
-        { user: 'John', message: 'Well hello there' },
-        { user: 'Rose', message: 'Oh hi, what are you doing?' }
-      ],
+      messages: [],
       users: []
     };
   },
+
+  socket: null,
+
+  handleConnect: function () {
+    var nickname = ['john', 'rose', 'elroy', 'steve', 'allison', 'gertie']
+                   [Math.floor(Math.random() * 6)];
+
+    this.socket.emit('setNickname', nickname);
+    console.info('Nickname:', nickname);
+  },
+
+  handleSubmit: function (e) {
+    var input = e.target.value;
+    if (input) {
+      this.socket.emit('newMessage', {message: input});
+    }
+    e.target.value = '';
+  },
+
+  initConversation: function (messages) {
+    this.setState({
+      messages: this.state.messages.concat(messages)
+    });
+  },
+  displayMessage: function (message) {
+    console.log(message);
+    this.setState({
+      messages: this.state.messages.concat([message])
+    });
+  },
+
+  updateUsers: function (data) {
+    this.setState({
+      users: data
+    });
+  },
+
+  componentDidMount: function () {
+    var socket = this.socket = window.io.connect(this.props.url);
+    socket.on('connect', this.handleConnect);
+    socket.on('pushMessage', this.displayMessage);
+    socket.on('initConversation', this.initConversation);
+    socket.on('updateUserList', this.updateUsers);
+  },
+
+  render: function () {
+    return (
+      <Display {...this.state} />
+    );
+  }
+});
+
+var Display = React.createClass({  // eslint-disable-line no-unused-vars
   render: function () {
     return (
       <div id="chatApp">
         <div>App</div>
-        <MessageList messages={this.state.messages} />
+        <MessageList messages={this.props.messages} />
         <UsersList />
       </div>
     );
@@ -56,5 +107,6 @@ var UsersList = React.createClass({  // eslint-disable-line no-unused-vars
   }
 });
 
-var chatApp = React.render(<App />, document.getElementById('app'));
-
+/* eslint-disable no-unused-vars*/
+var chatApp = React.render(<App url='http://localhost:1337' />, document.getElementById('app'));
+/* eslint-enable no-unused-vars*/
