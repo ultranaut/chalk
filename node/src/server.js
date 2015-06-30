ca176b7cfe3b3485a4c0b13e7f0328ac86869286
John Shearer <john@ultranaut.com>
2015-06-30 13:56:57 -0400

    Add some dummy conversation to start

--- node/src/server.js
+++ node/src/server.js
@@ -4,7 +4,29 @@ var io = require('socket.io');
 
 var chatServer = (function (io) {
   var connections = [];
-  var conversation = [];
+  var conversation = [
+    // dummy conversation already in progress...
+    {user: 'mike', message: 'this is mike here'},
+    {user: 'jeff', message: 'hey mike, it\'s jeff'},
+    {user: 'mike', message: 'oh cool'},
+    {user: 'jeff', message: 'yeah kind of'},
+    {user: 'steve', message: 'now im steve i guess'},
+    {user: 'earl', message: 'and im earl'},
+    {user: 'josef', message: 'roly-poly'},
+    {user: 'jose', message: 'really'},
+    {user: 'gertie', message: 'weee'},
+    {user: 'allison', message: 'boo'},
+    {user: 'ellie', message: 'so cool'},
+    {user: 'luis', message: 'yo amo'},
+    {user: 'roberto', message: 'we will walk the paved path'},
+    {user: 'sylvie', message: 'yoiks'},
+    {user: 'sylvie', message: 'oh noes'},
+    {user: 'sylvie', message: 'c\'mon'},
+    {user: 'sylvie', message: 'jsut jake'},
+    {user: 'matt', message: 'what\'s doing?'},
+    {user: 'matt', message: 'nothing'},
+    {user: 'lebron', message: 'what happens now?'}
+  ];
 
   var server = io.listen(1337);
   /*
