b9d5b5c952af07b947a259fcd1ec679b01f9fa12
John Shearer <john@ultranaut.com>
2021-04-14 21:47:01 -0400

    Add `no-var` rule to eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parser": "@babel/eslint-parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@babel"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@babel'],
+  rules: {
+    'no-var': 'error',
+  },
 };
