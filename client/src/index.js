1bd00e34ab770ea28a3e0be81f5a2b89aaaff994
John Shearer <john@ultranaut.com>
2021-04-19 18:52:37 -0400

    Wrap `App` with `Provider`, create dummy reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <App url="http://localhost:1337" />,
+  <Provider store={store}>
+    <App url="http://localhost:1337" />
+  </Provider>,
   document.querySelector('#root')
 );
